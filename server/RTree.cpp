36f2617d1be42d9a6d75fc4669b0d15f03a3d156
Daniel Palomino <dapalominop@gmail.com>
2018-08-10 19:05:15 -0500

    MBR class created and topics.

--- server/RTree.cpp
+++ server/RTree.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : RTree.cpp
-// Author      : Daniel Palomino
+// Author      : Daniel Palomino / Vittorino Mandujano
 // Version     : 0.1
 // Copyright   : Your copyright notice
 // Description : RTree Backend
@@ -9,19 +9,63 @@
 
 #include "RTree.h"
 
-RTree::RTree() {
-	return;
+RTree::RTree(int ord = 2)
+{
+	M = ord;
+	m = (ord + 1) / 2;
+	head = new MBR;
+	head->coMbr = NULL;
+	head->subMbr = NULL;
+}
+
+RTree::insert(vector<pair<int, int>> polygon)
+{
+	MBR* fig = new MBR(polygon);
+
+	if (head->subMbr == NULL && head->coMbr == NULL) {//la primerafigura insertada
+		head->subMbr = fig;
+		head->setXY(fig);//Modifica el MBR's del recorrido            
+	}
+	else {
+		Node* temp = head;
+		Node* temp2;
+		int area = 0;
+		//vector<MBR*> v; v.push_back(temp); se añade a una pila
+		while (temp->estado) {//busca el MBR que se ajuste a fig. y asignarlo
+			temp2 = temp;
+			area = temp->areaBetween(fig);
+			while (temp->coMbr != NULL) {
+				temp = temp->coMbr;
+				if ((temp->areaBetween(fig))<area) {
+					area = temp->areaBetween(fig);
+					temp2 = temp;
+				}
+			}
+			temp = temp2;
+			//v.push_back(temp); se añade a una pila
+			temp = temp->subMbr;//revisar,puede haber un error                        
+		}
+		temp = temp2;//insertar
+		temp2 = temp->subMbr;
+		temp->subMbr = fig;
+		fig->coMbr = temp2;
+		//===========================================
+		//todavia usamos temp
+		if (M<(temp->count())) {//verificar la cantidad para hacer el split
+			split();//el split
+			temp2 = head;// revisar!!!!                    
+		}
+
+
+		//Modificar MBR's del recorrido                             
+	}
 }
 
 bool RTree::split() {
 	return true;
 }
 
-bool RTree::insert(vector<pair<int, int>> polygon) {
-	return true;
-}
-
-bool RTree::mbr(vector<vector<pair<int, int>>>& mbrs) {
+bool RTree::getMBRs(vector<vector<pair<int, int>>>& mbrs) {
 	return true;
 }
 
--- server/RTree.h
+++ server/RTree.h
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : RTree.h
-// Author      : Daniel Palomino
+// Author      : Daniel Palomino / Vittorino Mandujano
 // Version     : 0.1
 // Copyright   : Your copyright notice
 // Description : RTree Backend
@@ -11,20 +11,24 @@
 #define RTREE_H
 
 #include <vector>
+#include <iostream>
+#include <cstdlib>
+#include "MBR.h"
 
 using namespace std;
 
-class RTree 
+class RTree
 {
 private:
-
+	MBR * head;
+	int M, m; //para saber cuantos hijos debe tener cada nodo   
 public:
-	RTree();
-	bool split();
-	bool insert(vector<pair<int,int>> polygon);//inserta polygon en el rtree
-	bool mbr(vector<vector<pair<int,int>>>& mbrs);//devuelve todos los MBRs del rtree
+	RTree(int ord = 3);
+	void insert(vector<pair<int, int>> polygon);//inserta polygon en el rtree
+	bool getMBRs(vector<vector<pair<int, int>>>& mbrs);//devuelve todos los MBRs del rtree
 	bool range(vector<pair<int, int>> polygon, vector<vector<pair<int, int>>>& objects);//retorna los objects dentro de polygon
 	bool nearest(int k, vector<pair<int, int>> point, vector<vector<pair<int, int>>>& objects);//retorn los k objects más cercanos a point
+	bool split();
 	bool deleteAll();//Elimina todos los objetos del rtree
 };
 
